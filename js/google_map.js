fb036da531e7d6030b9e48064f0256bd78245b49
Jane Wilson <janewilson90@users.noreply.github.com>
2018-10-17 19:15:17 +0100

    trying to get the bloody map to work

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(56.433573, -3.249930);
+    var myLatlng = new google.maps.LatLng(56.433057, -3.253631);
     // 39.399872
     // -8.224454
     
@@ -30,7 +30,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Fingask Castle'];
+    var addresses = ['Fingask Castle, Rait, Perth PH2 7SA'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
@@ -39,7 +39,7 @@ function init() {
             new google.maps.Marker({
                 position: latlng,
                 map: map,
-                icon: 'images/loc.png'
+                icon: '../images/loc.png'
             });
 
         });
