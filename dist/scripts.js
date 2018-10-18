4405ee5ea7c9afc467275199cdd2e92f5e182d4f
Jane Wilson <janeawilson1990@gmail.com>
2018-10-18 11:22:08 +0100

    removed stuff which was commented out anyway

--- dist/scripts.js
+++ dist/scripts.js
@@ -780,19 +780,6 @@ function init() {
         infowindow.open(map, marker);
         map.setCenter(marker.getPosition());
     });
-
-    // for (var x = 0; x < addresses.length; x++) {
-    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-    //         var p = data.results[0].geometry.location
-    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
-    //         new google.maps.Marker({
-    //             position: latlng,
-    //             map: map,
-    //             icon: '../images/loc.png'
-    //         });
-
-    //     });
-    // }
     
 }
 google.maps.event.addDomListener(window, 'load', init);
